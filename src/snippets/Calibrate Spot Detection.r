273121f257fd3008f8a1253edc72e6847dca350c
ushi <ushi@honkgong.info>
2014-04-15 14:33:59 +0200

    Renamed "Functions and Snippets/" -> snippets/

--- src/snippets/Calibrate Spot Detection.r
+++ src/snippets/Calibrate Spot Detection.r
@@ -9,17 +9,17 @@
 # 	preferences$summary.spot.minsize
 # 	preferences$summary.spot.maxsize
 # 	preferences$summary.spot.startlevel
-# 
+#
 # 	# preferences$summary.spot.minsize = 4
 # 	# preferences$summary.spot.maxsize = 10
 # 	# preferences$summary.spot.startlevel = 0.9
-# 
+#
 # 	preferences$summary.spot.minsize = 1
 # 	preferences$summary.spot.maxsize = 4
 # 	preferences$summary.spot.startlevel = 0.9
-# 
-# 
-# 	source("Functions and Snippets/f - Find  Overexpression spots - old.r")
+#
+#
+# 	source("snippets/f - Find  Overexpression spots - old.r")
 
 
 
@@ -39,18 +39,18 @@
 	preferences$group.spot.threshold = 0.88
 
 	source("lib/source/detect_spots_integral.r")
-	
+
 	colramp = colorRampPalette( c( "darkblue","blue","lightblue","green","yellow","red","darkred" ) )
 
-	
+
 	par(mfrow=c(2,2), mar=c(2,2,2,2))
 	image( matrix( GS.infos.overexpression$overview.map, preferences$dim.som1 ), col=colramp(1000) )
 	image( matrix( GS.infos.overexpression$overview.mask, preferences$dim.som1 ), col=colramp(1000) )
 	par( new=T )
 	plot( 0, type="n", axes=F, xlab="", ylab="", xlim=c(0,preferences$dim.som1), ylim=c(0,preferences$dim.som1), xaxs="i", yaxs="i" )
-	points( do.call( rbind, lapply( GS.infos.overexpression$spots, function(x) x$position ) ), pch=16, cex=1, col="black" )		
-	points( do.call( rbind, lapply( GS.infos.overexpression$spots, function(x) x$position ) ), pch=1, cex=1, col="white" )			
-	
+	points( do.call( rbind, lapply( GS.infos.overexpression$spots, function(x) x$position ) ), pch=16, cex=1, col="black" )
+	points( do.call( rbind, lapply( GS.infos.overexpression$spots, function(x) x$position ) ), pch=1, cex=1, col="white" )
+
 	length(GS.infos.overexpression$spots)
 
 
@@ -58,13 +58,13 @@
 	image( matrix( GS.infos.group.overexpression$overview.mask, preferences$dim.som1 ), col=colramp(1000) )
 	par( new=T )
 	plot( 0, type="n", axes=F, xlab="", ylab="", xlim=c(0,preferences$dim.som1), ylim=c(0,preferences$dim.som1), xaxs="i", yaxs="i" )
-	points( do.call( rbind, lapply( GS.infos.group.overexpression$spots, function(x) x$position ) ), pch=16, cex=1, col="black" )		
-	points( do.call( rbind, lapply( GS.infos.group.overexpression$spots, function(x) x$position ) ), pch=1, cex=1, col="white" )			
-	
+	points( do.call( rbind, lapply( GS.infos.group.overexpression$spots, function(x) x$position ) ), pch=16, cex=1, col="black" )
+	points( do.call( rbind, lapply( GS.infos.group.overexpression$spots, function(x) x$position ) ), pch=1, cex=1, col="white" )
+
 	length(GS.infos.group.overexpression$spots)
-	
-	
-	
+
+
+
 
 
 
@@ -90,21 +90,21 @@ source("lib/source/gene_lists.r")
 source("lib/source/summary_sheets_integral.r")
 
 dir.create( paste( files.name, "- Results/3rd lvl Spot Analysis" ), showWarnings=F )
-source("lib/source/3rd_lvl_chromosomal_enrichment.r")	
-source("lib/source/3rd_lvl_summary_sheets.r")		
-source("lib/source/3rd_lvl_networks.r")	
+source("lib/source/3rd_lvl_chromosomal_enrichment.r")
+source("lib/source/3rd_lvl_summary_sheets.r")
+source("lib/source/3rd_lvl_networks.r")
 
-source("lib/source/html_integral_summary.r")	
+source("lib/source/html_integral_summary.r")
 
 source("lib/source/workspace_cleanup.r")
 save.image( paste( files.name, ".RData" , sep="" ) )
 
 
-#source("lib/source/3rd_lvl_overexpression_genenet.r")	
+#source("lib/source/3rd_lvl_overexpression_genenet.r")
 source("lib/source/signature_sets.r")
 
 
 
-	
+
 
 
