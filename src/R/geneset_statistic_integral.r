2e3b02158dbdc4d33e78d46af370cb3de1b23ebf
ushi <ushi@honkgong.info>
2014-05-16 16:18:03 +0200

    Improved parallelization: genesetStatisticIntegral

--- src/R/geneset_statistic_integral.r
+++ src/R/geneset_statistic_integral.r
@@ -1,61 +1,43 @@
 pipeline.genesetStatisticIntegral <- function()
 {
+  spot.fisher.p <- function(spot)
+  {
+    spot$Fisher.p <- GeneSet.Fisher(unique(na.omit(gene.ids[spot$genes])),
+                                    unique.protein.ids, gs.def.list, sort=T)
+
+    return(spot)
+  }
+
   ### init parallel computing ###
   cl <- makeCluster(preferences$max.parallel.cores)
 
-  ### perform GS analysis ###
-  for (i in 1:length(spot.list.overexpression$spots))
-  {
-    geneset.ids <- unique(na.omit(gene.ids[spot.list.overexpression$spots[[i]]$genes]))
-
-    spot.list.overexpression$spots[[i]]$Fisher.p <<-
-      GeneSet.Fisher(geneset.ids, unique.protein.ids, gs.def.list, sort=T, cluster=cl)
-  }
+  spot.list.overexpression$spots <<-
+    parLapply(cl, spot.list.overexpression$spots, spot.fisher.p)
 
   util.progress(96, 100)
 
-  for (i in 1:length(spot.list.underexpression$spots))
-  {
-    geneset.ids <- unique(na.omit(gene.ids[spot.list.underexpression$spots[[i]]$genes]))
-
-    spot.list.underexpression$spots[[i]]$Fisher.p <<-
-      GeneSet.Fisher(geneset.ids, unique.protein.ids, gs.def.list, sort=T, cluster=cl)
-  }
+  spot.list.underexpression$spots <<-
+    parLapply(cl, spot.list.underexpression$spots, spot.fisher.p)
 
   util.progress(97, 100)
 
   if (length(spot.list.correlation$spots) > 0)
   {
-    for (i in 1:length(spot.list.correlation$spots))
-    {
-      geneset.ids <- unique(na.omit(gene.ids[spot.list.correlation$spots[[i]]$genes]))
-
-      spot.list.correlation$spots[[i]]$Fisher.p <<-
-        GeneSet.Fisher(geneset.ids, unique.protein.ids, gs.def.list, sort=T, cluster=cl)
-    }
+    spot.list.correlation$spots <<-
+      parLapply(cl, spot.list.correlation$spots, spot.fisher.p)
   }
 
   util.progress(98, 100)
 
-  for (i in 1:length(spot.list.kmeans$spots))
-  {
-    geneset.ids <- unique(na.omit(gene.ids[spot.list.kmeans$spots[[i]]$genes]))
-
-    spot.list.kmeans$spots[[i]]$Fisher.p <<-
-      GeneSet.Fisher(geneset.ids, unique.protein.ids, gs.def.list, sort=T, cluster=cl)
-  }
+  spot.list.kmeans$spots <<-
+    parLapply(cl, spot.list.kmeans$spots, spot.fisher.p)
 
   util.progress(99, 100)
 
   if (length(unique(group.labels)) > 1)
   {
-    for (i in 1:length(spot.list.group.overexpression$spots))
-    {
-      geneset.ids <- unique(na.omit(gene.ids[spot.list.group.overexpression$spots[[i]]$genes]))
-
-      spot.list.group.overexpression$spots[[i]]$Fisher.p <<-
-        GeneSet.Fisher(geneset.ids, unique.protein.ids, gs.def.list, sort=T, cluster=cl)
-    }
+    spot.list.group.overexpression$spots <<-
+      parLapply(cl, spot.list.group.overexpression$spots, spot.fisher.p)
   }
 
   util.progress.terminate()
