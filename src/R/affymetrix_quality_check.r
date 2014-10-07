99b097ef755b61bd64e44fe4ad4583e4f394c7d3
henry <wirth@izbi.uni-leipzig.de>
2014-10-07 13:40:15 +0200

    !!! bugfix affymetrix_quality_check.r

--- src/R/affymetrix_quality_check.r
+++ src/R/affymetrix_quality_check.r
@@ -3,7 +3,7 @@ pipeline.affymetrixQualityCheck <- function()
    
   spike.in.controls = c("AFFX-LysX-3_at","AFFX-PheX-3_at","AFFX-ThrX-3_at","AFFX-DapX-3_at")
   
-  if( all( spike.in.controls %in% rownames(indata.original) ) )
+  if( all( spike.in.controls %in% rownames(indata) ) )
   {  
     
     for( i in spike.in.controls )
@@ -11,12 +11,12 @@ pipeline.affymetrixQualityCheck <- function()
       par(mfrow=c(2,1))
       par(mar=c(5,3,3,2))
       
-      barplot( indata.original[i,], col=group.colors, main=paste("Intensity of sample preparation spike-in control",i), names.arg=if(ncol(indata)<80) colnames(indata) else rep("",ncol(indata)), las=2, cex.main=1, cex.lab=1, cex.axis=0.8, cex.names=0.6, border = ifelse(ncol(indata)<80,"black",NA), ylim=range( indata.original[i,] )*c(0.99,1.01), xpd=FALSE )	
+      barplot( indata[i,], col=group.colors, main=paste("Intensity of sample preparation spike-in control",i), names.arg=if(ncol(indata)<80) colnames(indata) else rep("",ncol(indata)), las=2, cex.main=1, cex.lab=1, cex.axis=0.8, cex.names=0.6, border = ifelse(ncol(indata)<80,"black",NA), ylim=range( indata[i,] )*c(0.99,1.01), xpd=FALSE )	
       box()
       
       if( length( unique(group.labels) ) > 1 )
       {
-        mean.boxes <- by( indata.original[i,], group.labels, c )[ unique( group.labels ) ]
+        mean.boxes <- by( indata[i,], group.labels, c )[ unique( group.labels ) ]
         par(mar=c(5,3,0,2))
         boxplot( mean.boxes, col=groupwise.group.colors, las=2, main="", cex.main=1, cex.axis=0.8, xaxt="n" )
         axis( 1, 1:length(groupwise.group.colors), unique(group.labels), las=2, cex.axis=0.8 )
@@ -27,9 +27,9 @@ pipeline.affymetrixQualityCheck <- function()
   
   
   
-  if( all( c("AFFX-HUMGAPDH/M33197_3_at","AFFX-HUMGAPDH/M33197_5_at") %in% rownames(indata.original) ) )
+  if( all( c("AFFX-HUMGAPDH/M33197_3_at","AFFX-HUMGAPDH/M33197_5_at") %in% rownames(indata) ) )
   {  
-    ratio.3.5.GAPDH <- indata.original["AFFX-HUMGAPDH/M33197_3_at",] / indata.original["AFFX-HUMGAPDH/M33197_5_at",]
+    ratio.3.5.GAPDH <- indata["AFFX-HUMGAPDH/M33197_3_at",] / indata["AFFX-HUMGAPDH/M33197_5_at",]
     
     par(mfrow=c(2,1))
     par(mar=c(5,3,3,2))
@@ -51,9 +51,9 @@ pipeline.affymetrixQualityCheck <- function()
   
   
   
-  if( all( c("AFFX-HSAC07/X00351_3_at","AFFX-HSAC07/X00351_5_at") %in% rownames(indata.original) ) )
+  if( all( c("AFFX-HSAC07/X00351_3_at","AFFX-HSAC07/X00351_5_at") %in% rownames(indata) ) )
   {	
-    ratio.3.5.betaact <- indata.original["AFFX-HSAC07/X00351_3_at",] / indata.original["AFFX-HSAC07/X00351_5_at",]
+    ratio.3.5.betaact <- indata["AFFX-HSAC07/X00351_3_at",] / indata["AFFX-HSAC07/X00351_5_at",]
     
     par(mfrow=c(2,1))
     par(mar=c(5,3,3,2))
@@ -75,15 +75,15 @@ pipeline.affymetrixQualityCheck <- function()
   
    
   
-  if( all( c("AFFX-BioB-3_at","AFFX-BioC-3_at","AFFX-BioDn-3_at","AFFX-CreX-3_at") %in% rownames(indata.original) ) )
+  if( all( c("AFFX-BioB-3_at","AFFX-BioC-3_at","AFFX-BioDn-3_at","AFFX-CreX-3_at") %in% rownames(indata) ) )
   {  
       
     par(mfrow=c(1,1))
     par(mar=c(3,3,3,3))
-    plot( 0, type="n", xlim=c(1,4), ylim=range(c(indata.original["AFFX-BioB-3_at",],
-                                                 indata.original["AFFX-BioC-3_at",],
-                                                 indata.original["AFFX-BioDn-3_at",],
-                                                 indata.original["AFFX-CreX-3_at",] )), xlab="", ylab="", axes=FALSE, main="Intensities of spike-in controls" )
+    plot( 0, type="n", xlim=c(1,4), ylim=range(c(indata["AFFX-BioB-3_at",],
+                                                 indata["AFFX-BioC-3_at",],
+                                                 indata["AFFX-BioDn-3_at",],
+                                                 indata["AFFX-CreX-3_at",] )), xlab="", ylab="", axes=FALSE, main="Intensities of spike-in controls" )
     
     axis(2,las=2)
     axis(1,c(1:4),c("bioB","bioC","bioD","creX"))
@@ -91,10 +91,10 @@ pipeline.affymetrixQualityCheck <- function()
     
     for( m in seq(ncol(indata)) )
     {
-      lines( c( indata.original["AFFX-BioB-3_at",m],
-                indata.original["AFFX-BioC-3_at",m],
-                indata.original["AFFX-BioDn-3_at",m],
-                indata.original["AFFX-CreX-3_at",m] ),
+      lines( c( indata["AFFX-BioB-3_at",m],
+                indata["AFFX-BioC-3_at",m],
+                indata["AFFX-BioDn-3_at",m],
+                indata["AFFX-CreX-3_at",m] ),
              col=group.colors[m] )
     }
     
