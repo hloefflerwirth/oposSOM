213c6944617f61576ec21097fadcf67977809094
henry <wirth@izbi.uni-leipzig.de>
2015-03-13 17:43:20 +0100

    GS analysis integral un-paralleled

--- src/R/geneset_statistic_integral.r
+++ src/R/geneset_statistic_integral.r
@@ -8,40 +8,30 @@ pipeline.genesetStatisticIntegral <- function()
     return(spot)
   }
 
-  ### init parallel computing ###
-  cl <- makeCluster(preferences$max.parallel.cores)
-
-  spot.list.overexpression$spots <<-
-    parLapply(cl, spot.list.overexpression$spots, spot.fisher.p)
+  spot.list.overexpression$spots <<- lapply( spot.list.overexpression$spots, spot.fisher.p)
 
   util.progress(96, 100)
 
-  spot.list.underexpression$spots <<-
-    parLapply(cl, spot.list.underexpression$spots, spot.fisher.p)
+  spot.list.underexpression$spots <<- lapply( spot.list.underexpression$spots, spot.fisher.p)
 
   util.progress(97, 100)
 
   if (length(spot.list.correlation$spots) > 0)
   {
-    spot.list.correlation$spots <<-
-      parLapply(cl, spot.list.correlation$spots, spot.fisher.p)
+    spot.list.correlation$spots <<- lapply( spot.list.correlation$spots, spot.fisher.p)
   }
 
   util.progress(98, 100)
 
-  spot.list.kmeans$spots <<-
-    parLapply(cl, spot.list.kmeans$spots, spot.fisher.p)
+  spot.list.kmeans$spots <<- lapply( spot.list.kmeans$spots, spot.fisher.p)
 
   util.progress(99, 100)
 
   if (length(unique(group.labels)) > 1)
   {
-    spot.list.group.overexpression$spots <<-
-      parLapply(cl, spot.list.group.overexpression$spots, spot.fisher.p)
+    spot.list.group.overexpression$spots <<- lapply( spot.list.group.overexpression$spots, spot.fisher.p)
   }
 
   util.progress.terminate()
 
-  ### stop parallel computing ###
-  try({ stopCluster(cl) }, silent=TRUE)
 }
