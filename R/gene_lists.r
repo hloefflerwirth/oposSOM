be236b75e5e04598d6ccbde821f384ccf7ebd4d7
hloefflerwirth <wirth@izbi.uni-leipzig.de>
2022-08-31 09:09:10 +0200

    module label added to gene localization table

--- R/gene_lists.r
+++ R/gene_lists.r
@@ -2,11 +2,21 @@ pipeline.geneLists <- function(env)
 {
   
   #### Gene Localization Table ####
+  
+  spot.list <- env[[paste("spot.list.", env$preferences$standard.spot.modules,sep="")]]
+  gene.module.info <- rep("",nrow(env$indata))
+  names(gene.module.info) <- rownames(env$indata)
+  for( x in names(spot.list$spots) )
+  {
+    gene.module.info[ spot.list$spots[[x]]$genes ] <- x
+  }
+  
   o <- order(env$som.result$feature.BMU)
   out <- data.frame(ID=rownames(env$indata)[o],
                     Symbol=env$gene.info$names[o],
                     MeanExpression=env$indata.gene.mean[o],
                     Metagene=env$gene.info$coordinates[o],
+                    Module=gene.module.info[o],
                     Chromosome=paste( env$gene.info$chr.name[rownames(env$indata)[o]], env$gene.info$chr.band[rownames(env$indata)[o]]),
                     Description=env$gene.info$descriptions[o])
   
